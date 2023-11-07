7a1b32c31cda325469736706dd671a68e78a465a
fbettir <betti.farkas2@gmail.com>
2023-11-07 19:40:23 +0100

    backend to higher file level

--- VolleyballManagementAppBackend/VolleyballManagementAppBackend/Entities/Gender.cs
+++
@@ -1,9 +0,0 @@
-namespace VolleyballManagementAppBackend.Entities
-{
-  public enum Gender
-  {
-    Man = 0,
-    Woman = 1,
-    Other = 2,
-  }
-}
--- VolleyballManagementAppBackend/VolleyballManagementAppBackend/Entities/Role.cs
+++
@@ -1,8 +0,0 @@
-namespace VolleyballManagementAppBackend.Entities
-{
-  public enum Role
-  {
-    Administrator = 0,
-    Coach = 1,
-  }
-}
