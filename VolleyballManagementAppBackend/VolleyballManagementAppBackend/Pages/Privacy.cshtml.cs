2303e7afd21748a12ff3d2390b67cfa474b50c7c
fbettir <betti.farkas2@gmail.com>
2023-11-07 19:42:14 +0100

    updating file structure

---
+++ VolleyballManagementAppBackend/VolleyballManagementAppBackend/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VolleyballManagementAppBackend.Pages
+{
+  public class PrivacyModel : PageModel
+  {
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+      _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+  }
+}
