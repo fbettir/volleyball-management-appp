134f8ad5b050733916cdd71732e4cf95eb97872d
Bernadett Farkas <79795106+fbettir@users.noreply.github.com>
2023-12-14 15:04:31 +0100

    dialog components fix

--- VolleyballManagementAppFrontend/src/app/components/update-dialog/update-dialog.component.ts
+++ VolleyballManagementAppFrontend/src/app/components/update-dialog/update-dialog.component.ts
@@ -7,6 +7,7 @@ import { PlayerDetails } from 'src/app/models/player-details';
 import { PlayerDetailsWithName } from 'src/app/models/player-details-with-name';
 import { Post } from 'src/app/models/post';
 import { Role } from 'src/app/models/role';
+import { TicketPass } from 'src/app/models/ticket-pass';
 import { User } from 'src/app/models/user';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -16,25 +17,43 @@ import { v4 as uuidv4 } from 'uuid';
   styleUrls: ['./update-dialog.component.scss'],
 })
 export class UpdateDialogComponent {
+
   constructor(
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<UpdateDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: PlayerDetailsWithName,
-  ) {}
+  ) {
+      // this.addPlayerForm.value.posts.push(0);
+      console.log(this.addPlayerForm);
+      console.log(this.dataSource.data[0]);
+      // this.addPlayerForm.controls.posts.setValue([Post.Receiver] as any); 
+    console.log(this.posts);
+  }
 
   dataSource = new MatTableDataSource<PlayerDetailsWithName>([this.data]);
 
-  posts: Post[] = this.dataSource.data[0].posts;
+  posts = this.dataSource.data[0].posts.map(String);
 
-  addPlayerForm = this.formBuilder.group({
-    ticketPass: this.dataSource.data[0].ticketPass,
-    playerNumber: this.dataSource.data[0].playerNumber,
-    posts: this.posts,
+  addPlayerForm = this.formBuilder.group<{
+    name: string,
+    birthday: Date,
+    phone: string,
+    playerNumber: number,
+    posts: Post[],
+    ticketPass: TicketPass,
+    gender: Gender
+  }>({
     name: this.dataSource.data[0].name,
-    phone: this.dataSource.data[0].phone,
     birthday: this.dataSource.data[0].birthday,
+    phone: this.dataSource.data[0].phone,
+    playerNumber: this.dataSource.data[0].playerNumber,
+    posts: [],
+    ticketPass: this.dataSource.data[0].ticketPass!,
     gender: this.dataSource.data[0].gender,
-  });
+  })
+  // posts: Post[] = this.dataSource.data[0].posts;
+
+
 
   onNoClick(): void {
     console.log(this.dialogRef);
@@ -49,7 +68,7 @@ export class UpdateDialogComponent {
       name: name!,
       ticketPass: ticketPass!,
       playerNumber: playerNumber!,
-      posts: [posts!],
+      posts: [],
       phone: phone!,
       birthday: birthday!,
       gender: gender! as Gender,
