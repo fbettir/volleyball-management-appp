421e908a035369b20597ac9b30bf61c06c828fb3
fbettir <betti.farkas2@gmail.com>
2025-04-17 00:36:45 +0200

    teams page almost styled

--- VolleyballManagementAppFrontend/src/app/shared/team-search-bar/team-search-bar.component.ts
+++ VolleyballManagementAppFrontend/src/app/shared/team-search-bar/team-search-bar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-team-search-bar',
   standalone: true,
+  imports: [FormsModule], 
   templateUrl: './team-search-bar.component.html',
   styleUrls: ['./team-search-bar.component.scss']
 })
 export class TeamSearchBarComponent {
   // Add @Output or search logic later if needed
+  searchText: string = '';
+  selectedFilter: string = 'name';
 }
