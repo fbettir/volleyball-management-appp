834aa40b163198995dc2de4aec9dc9ba7c5a34a7
fbettir <betti.farkas2@gmail.com>
2025-05-06 18:59:10 +0200

    training serach bar component

--- VolleyballManagementAppFrontend/src/app/components/training-search-bar/training-search-bar.component.ts
+++ VolleyballManagementAppFrontend/src/app/components/training-search-bar/training-search-bar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-training-search-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], 
   templateUrl: './training-search-bar.component.html',
   styleUrl: './training-search-bar.component.scss'
 })
 export class TrainingSearchBarComponent {
+  searchText: string = '';
+  selectedFilter: string = 'team';
 
+  @Output() searchChanged = new EventEmitter<{ text: string; filter: string }>();
+  
+  onSearchChange() {
+    this.searchChanged.emit({
+      text: this.searchText.trim().toLowerCase(),
+      filter: this.selectedFilter
+    });
+  }
 }
+ 
--- VolleyballManagementAppFrontend/src/app/pages/trainings-page/trainings-page.component.ts
+++ VolleyballManagementAppFrontend/src/app/pages/trainings-page/trainings-page.component.ts
@@ -1,24 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { TeamSearchBarComponent } from 'src/app/components/team-search-bar/team-search-bar.component';
 import { TrainingCardComponent } from 'src/app/components/training-card/training-card.component';
+import { TrainingSearchBarComponent } from 'src/app/components/training-search-bar/training-search-bar.component';
 import { Training } from 'src/app/models/training';
 import { TrainingService } from 'src/app/services/training.service';
 
 @Component({
   selector: 'app-trainings-page',
   standalone: true,
-  imports: [TeamSearchBarComponent, TrainingCardComponent, CommonModule],
+  imports: [TrainingSearchBarComponent, TrainingCardComponent, CommonModule],
   templateUrl: './trainings-page.component.html',
   styleUrl: './trainings-page.component.scss'
 })
 export class TrainingsPageComponent {
   trainings: Training[] = [];
+  filteredTrainings: Training[] = [];
 
   constructor(trainingService: TrainingService){
     trainingService.getAllTrainings().subscribe((t) => {
       console.log('Loaded trainings:', t);
       this.trainings = t;
+      this.filteredTrainings = t;
     })
   }
+  
+  onSearchChanged(search: { text: string; filter: string }) {
+    const { text, filter } = search;
+  
+    this.filteredTrainings = this.trainings.filter(training => {
+      if (!text) return true;
+  
+      if (filter === 'team') {
+        return training.team.name.toLowerCase().includes(text);
+      }
+  
+      if (filter === 'location') {
+        return training.location.name.toLowerCase().includes(text);
+      }
+  
+      return false;
+    });
+  }
 }
+
+
+ 
